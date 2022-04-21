a51a507b043d350929d8e76ce8f07fa666c373b5
Ismaelo107 <ismaeloeg@iescastelar.com>
2022-04-21 10:59:32 +0200

    405 casi

--- src/AceptaElReto/Problema149/P405.java
+++ src/AceptaElReto/Problema149/P405.java
@@ -4,18 +4,38 @@ import java.util.Scanner;
 
 public class P405 {
     public static void main(String[] args) {
+        /*
+        int casos = sc.nextInt(),
+        while (casos != 0) {
+            casos--;}
+            */
 
         Scanner sc = new Scanner(System.in);
-        int ini = 1, num = sc.nextInt(),next, fin;
+        int ini = sc.nextInt();
+        int num = -1;
+        int next;
+        int aux = 0;
+        String result;
+        boolean consecutivo=false;
+
 
         while (num != 0) {
             num = sc.nextInt();
-            next =ini+num;
-            System.out.println(num);
-            System.out.println(next);
+            next = num + 1;
+            if (next - num == 1 && num !=0) {
+                if (next !=0){
+                    aux = next-1 ;
+                    result= ini+"-"+aux;
 
+                } else{
+                    ini=
+                }
+            }
         }
+
+
 
 
     }
 }
+
